b0e85d7617af9b35560fdbcd32ad04d55ff86698
Emimint <90863470+Emimint@users.noreply.github.com>
2023-05-24 16:17:53 -0400

    set game logic

--- src/jeux/Jeu.java
+++ src/jeux/Jeu.java
@@ -1,5 +1,27 @@
 package jeux;
 
 public class Jeu {
+	
+	/*
+	 * Implementation du jeu:
+	 * 
+	 * J'ai choisi pour l'instant Chuck-A-Luck
+	 * 
+	 * 
+	 * nbrdeJoueurs: illimites
+	 * nbr Des: 3
+	 * 
+	 * optionDeMise: 1 a 6
+	 * 
+	 * Regles: Le joueur choisit un option de mise.
+	 * 
+	 * gains: 
+	 * 		- 1 x mise si 1 de apparait
+	 * 		- 2 x mises si 2 des appaissent
+	 * 		- 3 x mises si 3 des appaissent
+	 * 
+	 * 
+	 * 
+	 * */
 
 }
