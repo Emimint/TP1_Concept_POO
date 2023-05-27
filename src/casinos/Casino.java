9a110a37f3ae60b2f1342db4300ac3bbe4b66d0a
Emimint <90863470+Emimint@users.noreply.github.com>
2023-05-27 19:07:21 -0400

    start implementation of Casino class

--- src/casinos/Casino.java
+++ src/casinos/Casino.java
@@ -1,5 +1,64 @@
 package casinos;
 
+import jeux.Jeu;
+import joueurs.Joueur;
+
 public class Casino {
+
+	private String nom;
+	private Joueur[] joueurs;
+	private int maxJoueurs;
+	private int joueursPresents;
+	private Jeu jeu;
+	
+	public static int nbrCasinosCrees;
+	
+	public Casino() {
+		this(3);
+	}
+	
+	public Casino(int maxJoueurs) {
+		this("salle",maxJoueurs);
+		this.nom = this.nom + "_" + nbrCasinosCrees;
+	}
+	
+	public Casino(Casino autre) {
+		this(autre.nom,autre.maxJoueurs);
+		this.jeu = new Jeu(autre.jeu);
+	}
+	
+	public Casino(String nom, int maxJoueurs) {
+		nbrCasinosCrees++;
+		this.nom = nom;
+		this.maxJoueurs = maxJoueurs;
+		joueurs = new Joueur[maxJoueurs];
+		this.jeu = new Jeu();
+	}
+	
+	public boolean equals(Casino autre) {
+		if(this.maxJoueurs != autre.maxJoueurs) {
+			return false;
+		}
+		if(!this.jeu.equals(autre.jeu)) {
+			return false;
+		}	
+		return true;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getMaxJoueurs() {
+		return maxJoueurs;
+	}
+
+	public int getJoueursPresents() {
+		return joueursPresents;
+	}
 
 }
