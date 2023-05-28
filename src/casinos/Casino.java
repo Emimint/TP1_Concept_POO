2f592099688050f68c4e6ddc2c81f6dc5441eb77
Emimint <90863470+Emimint@users.noreply.github.com>
2023-05-28 14:46:45 -0400

    fix constructors and create standard methods

--- src/casinos/Casino.java
+++ src/casinos/Casino.java
@@ -10,39 +10,43 @@ public class Casino {
 	private int maxJoueurs;
 	private int joueursPresents;
 	private Jeu jeu;
-	
+
+// A gerer si j'ai le temps (ca implique d'atres methodes!!!):
+
+//	private int capital;
+//	private int gains;
+
 	public static int nbrCasinosCrees;
-	
+
 	public Casino() {
 		this(3);
 	}
-	
+
 	public Casino(int maxJoueurs) {
-		this("salle",maxJoueurs);
+		this("salle", maxJoueurs);
 		this.nom = this.nom + "_" + nbrCasinosCrees;
 	}
-	
+
 	public Casino(Casino autre) {
-		this(autre.nom,autre.maxJoueurs);
+		nbrCasinosCrees++;
+		this.nom = autre.nom;
+		this.maxJoueurs = autre.maxJoueurs;
+		joueursPresents = 0;
+		joueurs = new Joueur[autre.maxJoueurs];
 		this.jeu = new Jeu(autre.jeu);
+		
+		// Avant (a discuter):
+//		this(autre.nom, autre.maxJoueurs);
+//		this.jeu = new Jeu(autre.jeu);
 	}
-	
+
 	public Casino(String nom, int maxJoueurs) {
 		nbrCasinosCrees++;
 		this.nom = nom;
 		this.maxJoueurs = maxJoueurs;
+		joueursPresents = 0;
 		joueurs = new Joueur[maxJoueurs];
 		this.jeu = new Jeu();
-	}
-	
-	public boolean equals(Casino autre) {
-		if(this.maxJoueurs != autre.maxJoueurs) {
-			return false;
-		}
-		if(!this.jeu.equals(autre.jeu)) {
-			return false;
-		}	
-		return true;
 	}
 
 	public String getNom() {
@@ -61,4 +65,35 @@ public class Casino {
 		return joueursPresents;
 	}
 
+	public String getNomJeu() {
+		return jeu.getNom();
+	}
+
+	public String toString() {
+		
+		String chaine;
+		
+		chaine = String.format("Le casino s'apelle \"%s\". Il a une capacite maximale de %d joueurs. ", nom, maxJoueurs);
+		
+		if (joueursPresents > 0) {
+			chaine += String.format("\nIl a presentement %d joueur(s) dans cette salle. ", joueursPresents);
+			chaine += "\nLes voici: ";
+			for (int i = 0; i < joueursPresents; i++) {
+				chaine += "\n" + joueurs[i];
+			}
+		} else
+			chaine += "Cette salle actuellement est vide. ";
+
+		return chaine;
+	}
+
+	public boolean equals(Casino autre) {
+		if (this.maxJoueurs != autre.maxJoueurs) {
+			return false;
+		}
+		if (!this.jeu.equals(autre.jeu)) {
+			return false;
+		}
+		return true;
+	}
 }
