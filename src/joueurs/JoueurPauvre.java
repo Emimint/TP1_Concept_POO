9035c9c8a010712741710435e0bfb1a53f11933d
Emimint <90863470+Emimint@users.noreply.github.com>
2023-06-23 12:03:36 -0400

    implement JoueurPauvre methods

--- src/joueurs/JoueurPauvre.java
+++ src/joueurs/JoueurPauvre.java
@@ -1,9 +1,39 @@
 package joueurs;
 
-public class JoueurPauvre {
+import java.time.LocalDate;
 
+public class JoueurPauvre extends Joueur {
+	
 	public JoueurPauvre() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+	public JoueurPauvre(int capital) {
+		super(capital);
+	}
+
+	public JoueurPauvre(Joueur autre) {
+		super(autre);
+	}
+
+	public JoueurPauvre(String nom, int capital) {
+		super(nom, capital);
+	}
+
+	public JoueurPauvre(String nom) {
+		super(nom);
+	}
+
+	public void collecterCheque() {
+
+		int today = LocalDate.now().getDayOfMonth();
+
+		if (today == 1) {
+			System.out.println("C'est le premier du mois :D !");
+			setCapital(getCapital() + 700);
+			System.out.printf("Le joueur %s a maintenant un capital de %d$.\n", getNom(), getCapital());
+		} else 
+			System.out.println("Ce n'est pas encore le temps...");
+
+	}
 }
