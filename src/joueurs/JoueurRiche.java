ada14343ac513d74f2d385919508336ec65615db
Emimint <90863470+Emimint@users.noreply.github.com>
2023-06-23 12:27:21 -0400

    implement JoueurRiche methods

--- src/joueurs/JoueurRiche.java
+++ src/joueurs/JoueurRiche.java
@@ -1,9 +1,31 @@
 package joueurs;
 
-public class JoueurRiche {
+public class JoueurRiche extends Joueur{
 
 	public JoueurRiche() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+	public JoueurRiche(int capital) {
+		super(capital);
+	}
+
+	public JoueurRiche(Joueur autre) {
+		super(autre);
+	}
+
+	public JoueurRiche(String nom, int capital) {
+		super(nom, capital);
+	}
+
+	public JoueurRiche(String nom) {
+		super(nom);
+	}
+	
+	public void banqueRoute() {
+		System.out.println("Le gouvernement a tout saisi :( !");
+		setCapital(0);
+		quitterCasino();
+		System.out.printf("Le joueur %s a maintenant un capital de %d$...\n", getNom(), getCapital());
+	}
 }
