435b279c5748755f284dbbcad7c21a0520cae443
Emimint <90863470+Emimint@users.noreply.github.com>
2023-06-23 09:54:45 -0400

    implement CasinoLegal methods

--- src/casinos/CasinoLegal.java
+++ src/casinos/CasinoLegal.java
@@ -1,6 +1,39 @@
 package casinos;
 
+import joueurs.Joueur;
+
 public class CasinoLegal extends Casino {
+
+	private boolean peutJouer;
+	
+	public CasinoLegal() {
+		this(3);
+	}
+	
+	public CasinoLegal(Casino autre) {
+		super(autre);
+		peutJouer = true;
+	}
+
+	public CasinoLegal(int maxJoueurs) {
+		this("salle", maxJoueurs);
+		this.setNom(this.getNom() + "_" + nbrCasinosCrees);
+	}
+
+	public CasinoLegal(String nom, int maxJoueurs) {
+		super(nom, maxJoueurs);
+		peutJouer = true;
+	}
+
+	public CasinoLegal(String nomJeu, int nbrResultatsJeu, int champResultatJeu) {
+		this("casino_" + ++nbrCasinosCrees, 3, nomJeu, nbrResultatsJeu, champResultatJeu);
+		nbrCasinosCrees--;
+	}
+
+	public CasinoLegal(String nom, int maxJoueurs, String nomJeu, int nbrResultatsJeu, int champResultatJeu) {
+		super(nom, maxJoueurs, nomJeu, nbrResultatsJeu, champResultatJeu);
+		peutJouer = true;
+	}
 
 	@Override
 	public void payerImpots() {
@@ -8,4 +41,21 @@ public class CasinoLegal extends Casino {
 		super.setCapital((int) (super.getCapital() * 0.85));
 	}
 
+	public void debutSpectacle() {
+		System.out.println("Go, go, Gregory!!! Les joueurs s'arretent tous de jouer!");
+		peutJouer = false;
+	}
+
+	public void finSpectacle() {
+		System.out.println("Super concert!!! Les joueurs recommencent tous à jouer!");
+		peutJouer = true;
+	}
+
+	public void jouer(Joueur joueur, int mise) {
+		if (peutJouer) {
+			super.jouer(joueur, mise);
+		} else
+			System.out.printf("\nLe concert n'est pas fini, le joueur \"%s\" ne joue pas.\n", joueur.getNom());
+	}
+	
 }
