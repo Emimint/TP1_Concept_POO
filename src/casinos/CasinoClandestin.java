fe56327ee7e37ef8b5a43c1d314b56b36b2ed5ab
Emimint <90863470+Emimint@users.noreply.github.com>
2023-06-23 10:42:49 -0400

    implement CasinoClandestin methods

--- src/casinos/CasinoClandestin.java
+++ src/casinos/CasinoClandestin.java
@@ -1,15 +1,49 @@
 package casinos;
 
+import joueurs.Joueur;
+
 public class CasinoClandestin extends Casino {
+
+	public CasinoClandestin() {
+		super(3);
+	}
+
+	public CasinoClandestin(Casino autre) {
+		super(autre);
+	}
+
+	public CasinoClandestin(int maxJoueurs) {
+		super(maxJoueurs);
+	}
+
+	public CasinoClandestin(String nom, int maxJoueurs) {
+		super(nom, maxJoueurs);
+	}
+
+	public CasinoClandestin(String nomJeu, int nbrResultatsJeu, int champResultatJeu) {
+		super(nomJeu, nbrResultatsJeu, champResultatJeu);
+	}
+
+	public CasinoClandestin(String nom, int maxJoueurs, String nomJeu, int nbrResultatsJeu, int champResultatJeu) {
+		super(nom, maxJoueurs, nomJeu, nbrResultatsJeu, champResultatJeu);
+	}
 
 	@Override
 	public void payerImpots() {
-		int chance = (int)((100 * Math.random()) + 1);
+		int chance = (int) ((100 * Math.random()) + 1);
 		System.out.println("chance = " + chance);
 		if (chance >= 99) {
 			System.out.println("Pas de chance! C'est le jour des impots :( ...");
-			super.setCapital((int)(super.getCapital()*0.5));			
+			super.setCapital((int) (super.getCapital() * 0.5));
 		}
+	}
+
+	public void descentePolice() {
+		System.out.println("Oh-oh... C'est la police :o !!!");
+		while (getJoueursPresents() > 0) {
+			getJoueurs()[getJoueursPresents() - 1].quitterCasino();
+		}
+		System.out.println("Le casino est vide...");
 	}
 
 }
