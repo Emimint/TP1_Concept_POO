4e0d7b201a668078159c3105a4ce19dfded50992
Emimint <90863470+Emimint@users.noreply.github.com>
2023-06-28 14:23:38 -0400

    create Test class for Roulette and LotoQuebec

---
+++ src/jeux/Test.java
@@ -0,0 +1,53 @@
+package jeux;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Roulette roulette = new Roulette();
+		LotoQuebec loto = new LotoQuebec();
+
+//		for (String pari : Roulette.cheval) {
+//			System.out.println(pari);
+//		}
+//		for (String pari : Roulette.transversale) {
+//			System.out.println(pari);
+//		}
+//		for (String pari : Roulette.carre) {
+//			System.out.println(pari);
+//		}
+//		for (String pari : Roulette.sizain) {
+//			System.out.println(pari);
+//		}
+//
+//		System.out.println(Roulette.cheval.size());
+//		System.out.println(Roulette.transversale.size());
+//		System.out.println(Roulette.carre.size());
+//		System.out.println(Roulette.sizain.size());
+//		System.out.println(Roulette.chiffresRoue.length);
+
+//		int res = ((Roulette) roulette).getRandom(0, 5);
+//		((Roulette) roulette).getVoisins(Roulette.chiffresRoue, res);
+
+//		roulette.faireUnTirage();
+//		
+//		roulette.calculerGains(100);
+		
+//		roulette.afficherDetailsGains();
+//		
+//		System.out.println(roulette);
+		
+		
+//		System.out.println("\n-------------------------");
+//		
+//		System.out.println(loto);
+//		
+//		loto.faireUnTirage();
+//		
+//		loto.calculerGains(1000);
+		
+		
+		
+	}
+
+}
